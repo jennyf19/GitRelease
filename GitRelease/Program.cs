d15c9f78347320bf88c87bebadeb1545734704df
jennyf19 <v-jeferr@microsoft.com>
2016-09-30 19:39:01 -0700

    Added CommandLine interface

--- GitRelease/Program.cs
+++ GitRelease/Program.cs
@@ -10,58 +10,25 @@ namespace GitRelease
 {
     class Program
     {
-        //class to receive parsed values
-        internal class Options
-        {
-            [Option('g', "GitHubAccount", Required = true, HelpText = "Enter the GitHub Account Name for the repository you want to release")]
-            public string GitHubAccountName { get; set; }
-
-            [Option('r', "RepoName", Required = true, HelpText = "Enter the name of the repository you want to release")]
-            public string RepoName { get; set; }
-
-            [Option('t', "TagName", Required = true, HelpText = "Enter the tag name for the repository you want to release (ex. v1.0.0")]
-            public string TagName { get; set; }
-
-            [Option('p', "PersonalAccessToken", Required = true, HelpText = "Enter the personal access token for the account")]
-            public string PersonalAccessToken { get; set; }
-
-            [HelpOption]
-            public string GetUsage()
-            {
-                return HelpText.AutoBuild(this, current => HelpText.DefaultParsingErrorsHandler(this, current));
-            }
-        }
+        public static string GitHubAccountName;
+        public static string RepoName;
+        public static string TagName;
+        public static string PersonalAccessToken;
 
         static void Main(string[] args)
         {
-            var options = new Options();
-            if (!Parser.Default.ParseArguments(args, options))
-            {
-                Environment.Exit(Parser.DefaultExitCodeFail);
-                Console.ReadLine();
-            }
+            
+            CommandLineOptions commandLineOptions = new CommandLineOptions();
+            commandLineOptions.CommandLineInput(args, GitHubAccountName, RepoName, TagName, PersonalAccessToken);
             
             try
             {
-                string GitHubAccountName = options.GitHubAccountName;
-                string RepoName = options.RepoName;
-                string TagName = options.TagName;
-                string PersonalAccessToken = options.PersonalAccessToken;
 
                 TagNameFormatCheck tagNameFormatCheck = new TagNameFormatCheck();
                 tagNameFormatCheck.TagNameFormat(TagName);
 
-             if  (tagNameFormatCheck.TagNameFormat(TagName) == true)
+                if (tagNameFormatCheck.TagNameFormat(TagName) == true)
                 {
-
-                    Console.WriteLine("g|itHubAccount: " + options.GitHubAccountName);
-
-                    Console.WriteLine("r|epoName: " + options.RepoName);
-
-                    Console.WriteLine("t|agName: " + options.TagName);
-
-                    Console.WriteLine("p|ersonalAccessToken: " + options.PersonalAccessToken);
-
                     ReleaseAutomator releaseAutomator = new ReleaseAutomator();
                     releaseAutomator.AsyncAuthenticationMethod(GitHubAccountName, RepoName, TagName, PersonalAccessToken);
 
@@ -69,15 +36,18 @@ namespace GitRelease
                 }
                 else
                 {
-                    Console.WriteLine("Tag name format invalid");
+                    Console.WriteLine("Tag name format invalid: ", TagName);
                 }
 
             }
+
 
             catch (Exception a1)
             {
                 Console.WriteLine(a1);
             }
+
+
 
         }
 
---
+++ GitReleaseLibrary/ICommandLineOptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using CommandLine;
+using CommandLine.Text;
+
+namespace GitReleaseLibrary
+{
+    interface ICommandLineOptions
+    {
+        [Option('g', "GitHubAccountName", Required = true, HelpText = "Enter the GitHub Account Name for the repository you want to release")]
+        string GitHubAccountName { get; set; }
+
+        [Option('r', "RepoName", Required = true, HelpText = "Enter the name of the repository you want to release")]
+        string RepoName { get; set; }
+
+        [Option('t', "TagName", Required = true, HelpText = "Enter the tag name for the repository you want to release (ex. v1.0.0")]
+        string TagName { get; set; }
+
+        [Option('p', "PersonalAccessToken", Required = true, HelpText = "Enter the personal access token for the account")]
+        string PersonalAccessToken { get; set; }
+
+        string[] CommandLineInput(string[] args, out string[] outPut);
+
+    }
+}

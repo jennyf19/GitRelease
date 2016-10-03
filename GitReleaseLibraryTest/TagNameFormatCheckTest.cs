00137459619a619a7fd0d37c73e5e13a90d412d6
jennyf19 <v-jeferr@microsoft.com>
2016-10-02 21:39:32 -0700

    added unit tests for TagName format

---
+++ GitReleaseLibraryTest/TagNameFormatCheckTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GitReleaseLibrary;
+
+namespace GitReleaseLibraryTest
+{
+    [TestClass]
+    public class TagNameFormatCheckTest
+    {
+        [TestMethod]
+        public void TagNameFormatIncorrectWithTooManyDigitsTest()
+        {
+            TagNameFormatCheck tagName = new TagNameFormatCheck();
+            bool actual = tagName.TagNameFormat("v1.111.111");
+
+            Assert.AreEqual(false, actual);
+        }
+
+        [TestMethod]
+        public void TagNameFormatIncorrectWithTooFewDigitsTest()
+        {
+            TagNameFormatCheck tagName = new TagNameFormatCheck();
+            bool actual = tagName.TagNameFormat("v1.1.1");
+
+            Assert.AreEqual(false, actual);
+        }
+
+        [TestMethod]
+        public void TagNameFormatIncorrectWrongInitialLetterTest()
+        {
+            TagNameFormatCheck tagName = new TagNameFormatCheck();
+            bool actual = tagName.TagNameFormat("b1.11.11");
+
+            Assert.AreEqual(false, actual);
+        }
+
+        [TestMethod]
+        public void TagNameFormatTagMissingTest()
+        {
+            TagNameFormatCheck tagName = new TagNameFormatCheck();
+            bool actual = tagName.TagNameFormat("");
+
+            Assert.AreEqual(false, actual);
+        }
+
+        [TestMethod]
+        public void TagNameFormatInCorrectWithUpperCaseFirstLetterTest()
+        {
+            TagNameFormatCheck tagName = new TagNameFormatCheck();
+            bool actual = tagName.TagNameFormat("V1.11.11");
+
+            Assert.AreEqual(false, actual);
+        }
+
+        [TestMethod]
+        public void TagNameFormatCorrectTest()
+        {
+            TagNameFormatCheck tagName = new TagNameFormatCheck();
+            bool actual = tagName.TagNameFormat("v1.11.11");
+
+            Assert.AreEqual(true, actual);
+        }
+    }
+}
---
+++ UnitTestProject/TestTagNameFormat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using GitReleaseLibrary;
+
+namespace UnitTestProject
+{
+    public class TestTagNameFormat
+    {
+        [Fact]
+        public void TagNameFormatTest()
+        {
+            TagNameFormatCheck tagName = new TagNameFormatCheck();
+            tagName.TagNameFormat("v1.11.11");
+                        
+            Assert.Equal("v1.11.11", tagName.ToString());
+        }
+    }
+}
+/*
+ *  public string tagName { get; set; }
+
+        public bool TagNameFormat(string tagName)
+        {
+            Regex tagNamePattern = new Regex(@"^v[1-9]\.([0-9][0-9]\.)([0-9][0-9])$");
+
+            if (Regex.IsMatch(tagName, tagNamePattern.ToString()) && tagName != null)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+*/

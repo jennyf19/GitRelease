cb131a4c0b033f29d39ae1b9189fd81adf68f06e
jennyf19 <v-jeferr@microsoft.com>
2016-10-02 22:30:09 -0700

    Added comments on program.cs Added ReleaseAutomatorTest (not complete)

--- GitRelease/Program.cs
+++ GitRelease/Program.cs
@@ -11,28 +11,37 @@ namespace GitRelease
 
         static void Main(string[] args)
         {
+            //Dependency injection 
             var builder = new ContainerBuilder();
             builder.RegisterType<CommandLineInputParser>().As<ICommandLineInputParser>();
             builder.RegisterType<TagNameFormatCheck>().As<ITagNameFormatCheck>();
             builder.RegisterType<ReleaseAutomator>().As<IReleaseAutomator>();
             Container = builder.Build();
-        
+
             using (var scope = Container.BeginLifetimeScope())
             {
                 var release = scope.Resolve<IReleaseAutomator>();
             }
-                CommandLineInputParser options = new CommandLineInputParser();
 
+            //Take command line parameters as input
+            CommandLineInputParser options = new CommandLineInputParser();
+
+            //Check that all the parameters are present, if not exit
             if (!Parser.Default.ParseArguments(args, options))
             {
                 Environment.Exit(Parser.DefaultExitCodeFail);
                 Console.ReadLine();
             }
 
+            //Parse the parameters
             string gitHubAccountName = options.gitHubAccountName;
             string repoName = options.repoName;
             string tagName = options.tagName;
             string personalAccessToken = options.personalAccessToken;
+
+            //Determine if tagName is in the correct format (v1.11.11)
+            //If format passes, return true and continue to ReleaseAutomator
+            //If format does not pass, exit program
             try
             {
                 TagNameFormatCheck tagNameFormatCheck = new TagNameFormatCheck();
@@ -40,6 +49,8 @@ namespace GitRelease
 
                 if (tagNameFormatCheck.TagNameFormat(tagName) == true)
                 {
+                    //If the tagName returns true, initialize the ReleaseAutomator
+                    //ReleaseAutomator takes in the command line parameters and completes the repo release
                     ReleaseAutomator releaseAutomator = new ReleaseAutomator();
                     releaseAutomator.AsyncReleaseMethod(gitHubAccountName, repoName, tagName, personalAccessToken);
 
---
+++ GitReleaseLibraryTest/ReleaseAutomatorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GitReleaseLibrary;
+
+namespace GitReleaseLibraryTest
+{
+    [TestClass]
+    public class ReleaseAutomatorTest
+    {
+        [TestMethod]
+        public void AsyncReleaseMethodGetClientTest()
+        {
+            string gitHubAccountName = "jennyf19";
+            string repoName = "binaryTree";
+            string tagName = "v1.11.11";
+            string personalAccessToken = "127c0389c7a3ca4854bde31a22cf99f4285d4028";
+
+            ReleaseAutomator releaseAutomator = new ReleaseAutomator();
+            releaseAutomator.AsyncReleaseMethod(gitHubAccountName, repoName, tagName, personalAccessToken);
+
+            Assert.AreEqual();
+
+
+        }
+    }
+}
